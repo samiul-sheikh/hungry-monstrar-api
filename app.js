bad3ae0049645908fc366704096b7054218fe2ee
samiul-sheikh <sheikh.samiul1971@gmail.com>
2021-02-08 23:54:08 +0600

    view one item and see details

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ function searchMealData() {
         .then(data => {
             // console.log(data);
             displayMeal(data);
+            mealDetails(data);
         })
 }
 
@@ -26,4 +27,37 @@ function displayMeal(data) {
         `;
         document.getElementById("displayMeal").appendChild(mealsDiv);
     });
+}
+
+
+function mealDetails(data) {
+    const addEvent = document.getElementById("displayMeal");
+    addEvent.addEventListener('click', function () {
+        document.getElementById('foodDetails').innerHTML = '';
+        let i = 1
+        const mealName = event.target.parentNode.innerText;
+        data.meals.forEach(element => {
+            if (element.strMeal === mealName) {
+                const mealsDiv = document.createElement('div');
+                mealsDiv.classList.add('card', 'foodCard', 'col-6', 'm-2', 'p-2', 'mt-6', 'rounded-7');
+                mealsDiv.style.width = '20rem';
+                mealsDiv.innerHTML = `
+                    <img src=${element.strMealThumb} >
+                    <div class="card-body">
+                    <h3 class="card-text">${element.strMeal}</h3>
+                    <ul id="ingredients">
+                    </ul>
+                    </div>`;
+                document.getElementById('foodDetails').appendChild(mealsDiv);
+                while (i != 10) {
+                    let ingredient = 'strIngredient' + (i++);
+                    if (element[ingredient] == '')
+                        continue;
+                    const itemNew = document.createElement('li');
+                    itemNew.innerText = element[ingredient];
+                    document.getElementById('ingredients').appendChild(itemNew);
+                }
+            }
+        });
+    })
 }
